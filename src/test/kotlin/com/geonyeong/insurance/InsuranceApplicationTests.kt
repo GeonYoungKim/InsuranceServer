944a69271920fcfb717e1003b2b3e5d6fd220821
geonyoungkim <c2619zz@naver.com>
2018-09-27 16:49:15 +0900

    젠킨스 4.1build

--- src/test/kotlin/com/geonyeong/insurance/InsuranceApplicationTests.kt
+++ src/test/kotlin/com/geonyeong/insurance/InsuranceApplicationTests.kt
@@ -1,9 +1,11 @@
 package com.geonyeong.insurance
 
 import org.junit.Test
+import org.junit.Assert.assertThat
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.hamcrest.CoreMatchers.equalTo
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
@@ -11,6 +13,7 @@ class InsuranceApplicationTests {
 
 	@Test
 	fun contextLoads() {
+		assertThat("good",equalTo("good"))
 	}
 
 }
